cf927f22cda677b81f44345fb5bc64bedfaf8b25
clarissemilk <clarissesantana002@gmail.com>
2025-01-19 19:23:21 -0300

    Adiciona função de controle de LED na tecla 5

--- keypad_1.c
+++ keypad_1.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+
 // Definir os pinos GPIO para LEDs e Buzzer
 #define LED_RED_PIN 13
 #define LED_GREEN_PIN 11
@@ -135,9 +136,36 @@ void handle_key_4()
     }
 }
 
+//  acionar LEDs de forma aleatória
+void random_leds() {
+    // Gerar um número aleatório entre 0 e 2 (para escolher a cor do LED)
+    int ledColor = rand() % 3; // 0 - Vermelho, 1 - Verde, 2 - Azul
+
+    // Desligar todos os LEDs antes de acionar o próximo
+    gpio_put(LED_RED_PIN, 0);
+    gpio_put(LED_GREEN_PIN, 0);
+    gpio_put(LED_BLUE_PIN, 0);
+
+    // Ligar o LED correspondente
+    switch (ledColor) {
+        case 0: // Vermelho
+            gpio_put(LED_RED_PIN, 1);
+            break;
+        case 1: // Verde
+            gpio_put(LED_GREEN_PIN, 1);
+            break;
+        case 2: // Azul
+            gpio_put(LED_BLUE_PIN, 1);
+            break;
+        default:
+            break;
+    }
+}
+
 void handle_key_5()
 {
-    // Implementação futura
+     // Chama a função para acionar LEDs aleatórios
+    random_leds();
 }
 
 void handle_key_6()
